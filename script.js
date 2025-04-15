0e5d9123123d0c8dc47e0ffecc3fe0888f19aeb8
potato <45447595+wangffy@users.noreply.github.com>
2025-04-15 19:02:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
 Java.perform(function () {
-    let DmOrderActivity = Java.use("cn.damai.commonbusiness.seatbiz.sku.qilin.ui.NcovSkuActivity");
+    // 获取目标类
+    var SoLog = Java.use("cn.damai.utils.SoLog");
 
-    DmOrderActivity.onCreate.implementation = function(bundle) {
-        // 先调用原方法以确保逻辑完整性
-        this.onCreate(bundle);
+    // hook静态方法c
+    SoLog.c.overload('android.content.Context', 'long').implementation = function (context, j) {
+        // 你可以在这里打印参数
+        console.log("SoLog.c called, forcing return true, arguments:", context, j);
 
-        // 获取Intent中的extras
-        let intent = this.getIntent();
-        
-        let extras = intent.getExtras();
-
-        if (extras) {
-            console.log("Dumping Intent extras:");
-            let keySet = extras.keySet();
-            let iterator = keySet.iterator();
-
-            // 遍历所有键
-            while (iterator.hasNext()) {
-                let key = iterator.next();
-                try {
-                    // 获取值并处理可能的异常类型
-                    let value = extras.get(key);
-                    console.log(`  [${key}] => ${value} (类型: ${value ? value.getClass().getName() : 'null'})`);
-                } catch (e) {
-                    console.log(`  [${key}] => 读取错误: ${e}`);
-                }
-            }
-        } else {
-            console.log("No extras found in Intent");
-        }
+        // 强制返回 true
+        return true;
     };
-});
 
+    console.log("SoLog.c hooked");
+});
